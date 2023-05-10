a90f06e3c2e6124cd7f4c983f8de59b23c07fc1c
jeee0 <9402jee@naver.com>
2023-05-10 13:45:38 +0900

    Revert "230509_지영게시판작업"
    This reverts commit cc15c4ac2abe1843caefc3999ebe31ed42c2e58e.

--- 프로젝트/asia/src/main/java/com/asia/repository/NoticeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.asia.entity.Notice;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long>{
-	
-}
