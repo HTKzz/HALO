9040bd013983e3005cd5fb3932e06a49fe320f92
likeblue613 <yurika613@naver.com>
2023-05-30 09:14:00 +0900

    비우기

--- kim/프로젝트 작업용/asia/src/main/java/com/asia/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.asia.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
