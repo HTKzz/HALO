f1128a6dfc56a96945f56a6405f3329d640ce78e
DESKTOP-LV4EVO7\BIG603-08 <cws0094@naver.com>
2023-06-09 17:56:55 +0900

    .

--- choi/asia/src/main/java/com/asia/controller/VocController.java
+++ choi/asia/src/main/java/com/asia/controller/VocController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.asia.dto.SearchDto;
 import com.asia.dto.VocFormDto;
@@ -91,13 +92,18 @@ public class VocController {
 	// 상세보기
 	@GetMapping("/detail/{num}")
 	public String detailVoc(Model model, @PathVariable("num") Long num, Principal principal) {
+		
 		VocFormDto vocFormDto = vocService.getvocDtl(num);
 		vocService.updateCnt(num);
 		model.addAttribute("voc", vocFormDto);
-
-		String name = principal.getName();
-		Voc voc = vocService.findByNum(num);
-		model.addAttribute("username", name);
+		
+		if(principal != null) {
+			String name = principal.getName();
+			model.addAttribute("username", name);
+		}
+		
+		Voc voc = vocService.findByRealNum(num);
+		
 		model.addAttribute("writername", voc.getMember().getId());
 
 		return "board/voc/vocDetail";
@@ -105,8 +111,15 @@ public class VocController {
 
 	// 삭제
 	@GetMapping("/delete/{num}")
-	public String deleteVoc(@PathVariable Long num) throws Exception {
-		attachService.deleteAttach(num);
+	public String deleteVoc(@PathVariable Long num, Principal principal, RedirectAttributes re) throws Exception {
+		Voc voc = vocService.findByRealNum(num);
+		
+		if (!(voc.getMember().getId().equals(principal.getName()) || principal.getName().equals("admin"))) {
+			re.addFlashAttribute("errorMessage", "작성자 또는 관리자만 삭제가능합니다.");
+			return "redirect:/voc/detail/{num}";
+		}
+		
+		attachService.vocDeleteAttach(voc.getNum());
 		vocService.vocDelete(num);
 		
 		return "redirect:/voc/list";
@@ -114,9 +127,17 @@ public class VocController {
 
 	// 수정페이지 진입
 	@GetMapping(value = "/update/{num}") // url 경로변수는 {}로 표현
-	public String vocDtl(@PathVariable("num") Long num, Model model) {
+	public String vocDtl(@PathVariable("num") Long num, Model model, Principal principal, RedirectAttributes re) {
+		Voc voc = vocService.findByRealNum(num);
+		
+		if (!voc.getMember().getId().equals(principal.getName())) {
+			re.addFlashAttribute("errorMessage", "작성자만 수정가능합니다.");
+			return "redirect:/voc/detail/{num}";
+		}
+		
 		try {
 			VocFormDto vocFormDto = vocService.getvocDtl(num); // 조회한 데이터를 모델에 담아 뷰로 전달
+			System.out.println(vocFormDto);
 			model.addAttribute("vocFormDto", vocFormDto);
 		} catch (Exception e) { // 엔티티가 존재하지 않을 경우 에러메세지를 담아 등록페이지로 이동
 			model.addAttribute("errorMessage", "존재하지 않는 글 입니다.");
@@ -128,7 +149,8 @@ public class VocController {
 
 	// 수정시 작동
 	@PostMapping(value = "/update/{num}")
-	public String vocUpdate(@Valid VocFormDto vocFormDto, BindingResult bindingResult, @RequestParam("attachFile") List<MultipartFile> attachFileList, Model model) {
+	public String vocUpdate(@Valid VocFormDto vocFormDto, BindingResult bindingResult, @RequestParam("attachFile") List<MultipartFile> attachFileList, 
+			Model model, @PathVariable("num") Long num) {
 		if (bindingResult.hasErrors()) {
 			return "board/voc/vocForm";
 		}
@@ -145,7 +167,7 @@ public class VocController {
 	// 답글
 	@GetMapping("/reply/{num}")
 	public String replyForm(@PathVariable("num") Long num, Model model) {
-		Voc parentVoc = vocService.findByNum(num);
+		Voc parentVoc = vocService.findByRealNum(num);
 
 		model.addAttribute("vocFormDto", new VocFormDto());
 		model.addAttribute("num", num);
@@ -156,8 +178,7 @@ public class VocController {
 
 	@PostMapping("/reply/new")
 	public String newReplyVoc(@Valid VocFormDto vocFormDto, BindingResult bindingResult, Model model,
-			@RequestParam("attachFile") List<MultipartFile> attachFileList, @RequestParam("parentNo") Long parentNo,
-			@RequestParam("originNo") Long num, Principal principal) {
+			@RequestParam("attachFile") List<MultipartFile> attachFileList, @RequestParam("parentNo") Long parentNo, Principal principal) {
 
 		try {
 			String name = principal.getName();
